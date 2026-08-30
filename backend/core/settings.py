b106e299052040b1a42a7e78cdbf54ef62d2f632
M.Abdullah Rustam <143311068+AbdullahRustam99@users.noreply.github.com>
2026-08-30 19:33:25 +0500

    Update ALLOWED_HOSTS for production deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crmsystem-nn8y.vercel.app']
 
 
 # Application definition
